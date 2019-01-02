750a3bf3c03fb461ef263320b485152c380ada6c
Collin D'Mello <cldmello@yahoo.com>
2019-01-02 13:47:18 -0500

    Create types_demo.scala

---
+++ types_demo.scala
@@ -0,0 +1,46 @@
+/* Byte Type (must be in valid Byte range of values - (-2^7 to 2^7 - 1) ) */
+
+val minByteValue: Byte = -128
+val maxByteValue: Byte =  127
+
+/* Short Type (must be in valid Short range of values - (-2^15 to 2^15 - 1) ) */
+
+val minByteValue: Short = -32768
+val maxByteValue: Short =  32767
+
+/* Integer Type */
+val ten = 10
+
+/* Long Type */
+val anotherTenLong = 10l
+
+/* Float Type */
+val tenDotSomething = 10.23f
+val anotherTenDotSomething = 10.23F
+
+/* Double Type */
+val tenDouble = 10.23
+val anotherTenDouble = 10.23D
+val anotherTenDoubleD = 10.23d
+val tenDoubleExp = 1.023E1
+val anotherTenDoubleExp = 0.1023e2
+
+/* Character Type */
+val character = 'c'
+
+/* String Type */
+val helloWorld = "Hello World!"
+val escapeString = "\\\"\'"
+val rawEscapate = """\"'"""
+
+/* Boolean */
+val iAmTrue = true
+val iAmFalse = false
+
+
+
+
+
+
+
+
